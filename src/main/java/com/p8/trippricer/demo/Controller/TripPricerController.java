be39d4398498481796aae1b3264642e67227bc7c
Hossam-C <chenaoui.hossam@gmail.com>
2021-06-26 16:28:01 +0200

    Dockerisation

--- src/main/java/com/p8/trippricer/demo/Controller/TripPricerController.java
+++ src/main/java/com/p8/trippricer/demo/Controller/TripPricerController.java
@@ -2,6 +2,8 @@ package com.p8.trippricer.demo.Controller;
 
 
 import com.p8.trippricer.demo.Service.TripPricerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,22 +16,32 @@ import java.util.UUID;
 @RestController
 public class TripPricerController {
 
+    private Logger logger = LoggerFactory.getLogger(TripPricerController.class);
+
     @Autowired
     private TripPricerService tripPricerService;
 
     @GetMapping("/")
     public String tripPricerHome() {
+
+        logger.info("Controller : tripPricer Home");
+
         return "P8 tripPricerHome";
     }
 
     @GetMapping(value = "/getPrice")
     public List<Provider> getPrice (@RequestParam String apiKey, @RequestParam UUID attractionId, @RequestParam int adults, @RequestParam int children, @RequestParam int nightsStay, @RequestParam int rewardsPoints)  {
 
+        logger.info("Controller : getPrice");
+
         return tripPricerService.getPrice(apiKey,  attractionId,  adults,  children,  nightsStay,  rewardsPoints);
     }
 
     @GetMapping(value = "/getProviderName")
     public String getProviderName (@RequestParam String apiKey,@RequestParam int adults)  {
+
+        logger.info("Controller : getProviderName");
+
         return tripPricerService.getProviderName(apiKey,  adults);
 
     }
--- src/main/java/com/p8/trippricer/demo/Service/TripPricerServiceImpl.java
+++ src/main/java/com/p8/trippricer/demo/Service/TripPricerServiceImpl.java
@@ -1,6 +1,7 @@
 package com.p8.trippricer.demo.Service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import tripPricer.Provider;
 import tripPricer.TripPricer;
@@ -11,9 +12,9 @@ import java.util.UUID;
 @Service
 public class TripPricerServiceImpl implements TripPricerService{
 
+    private Logger logger = LoggerFactory.getLogger(TripPricerServiceImpl.class);
+
     private final TripPricer tripPricer;
-    //@Autowired
-    //private TripPricer tripPricer;
 
     public TripPricerServiceImpl() {
         this.tripPricer = new TripPricer();
@@ -23,11 +24,17 @@ public class TripPricerServiceImpl implements TripPricerService{
     @Override
     public List<Provider> getPrice(String apiKey, UUID attractionId, int adults, int children, int nightsStay, int rewardsPoints){
 
+        logger.debug("getPrice");
+
         return tripPricer.getPrice(apiKey,attractionId,adults,children,nightsStay,rewardsPoints);
     }
 
     @Override
     public String getProviderName(String apiKey, int adults){
+
+
+        logger.debug("getProviderName");
+
         return tripPricer.getProviderName(apiKey,adults);
     }
 }
